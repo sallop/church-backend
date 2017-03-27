7720500f68fe7346e31d067969e95e94515dc693
sallop <shiogai1987@yahoo.co.jp>
2017-03-27 19:34:08 +0900

    initial commit

---
+++ migrations/20170327081419-unnamed-migration.js
@@ -0,0 +1,33 @@
+'use strict';
+
+module.exports = {
+  up: function (queryInterface, Sequelize) {
+    /*
+      Add altering commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+    */
+    return queryInterface.createTable('users', {
+      id: Sequelize.INTEGER,
+      group: Sequelize.INTEGER,
+      name: Sequelize.STRING,
+      spirtual_name: Sequelize.STRING,
+      birthday: Sequelize.DATE,
+      telephone: Sequelize.STRING,
+      postcode: Sequelize.STRING,
+      address: Sequelize.STRING,
+      info: Sequelize.TEXT,
+    });
+  },
+
+  down: function (queryInterface, Sequelize) {
+    /*
+      Add reverting commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+    */
+    return queryInterface.dropTable('users');
+  }
+};
